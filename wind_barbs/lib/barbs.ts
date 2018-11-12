6b578e541d5cc3680c7538721f82a8977d80788e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-12 17:19:49 +0000

    separating source code from test code

--- wind_barbs/lib/barbs.ts
+++ wind_barbs/lib/barbs.ts
@@ -1,4 +1,76 @@
 "use strict";
+
+interface Barb {
+    flags: number;
+    full_barbs: number;
+    half_barbs: number;
+}
+
+export const draw = function(barb: Barb) {
+    let length = 7
+    let spacing = 0.875
+    let height = 2.8
+
+    // Special case for lone half barb
+    if ((barb.flags === 0) &&
+        (barb.full_barbs === 0) &&
+        (barb.half_barbs === 1)) {
+        let position = -length + (1.5 *spacing)
+        return [
+            [0, 0],
+            [-length, 0],
+            [position, 0],
+            [position - (spacing / 2), height / 2],
+            [position, 0],
+            [0, 0]
+        ]
+    }
+
+    let vertices = []
+    let position = -length
+    vertices.push([0, 0])
+    if (barb.flags > 0) {
+        for (let ib=0; ib<barb.flags; ib++) {
+            vertices.push([position, 0])
+            vertices.push([position + spacing, height])
+            vertices.push([position + (2 * spacing), 0])
+            position += 2 * spacing
+        }
+    }
+    if (barb.full_barbs > 0) {
+        for (let ib=0; ib<barb.full_barbs; ib++) {
+            vertices.push([position, 0])
+            vertices.push([position - spacing, height])
+            vertices.push([position, 0])
+            position += spacing
+        }
+    }
+    if (barb.half_barbs > 0) {
+        vertices.push([position, 0])
+        vertices.push([position - (spacing / 2), height/2])
+        vertices.push([position, 0])
+        position += spacing
+    }
+    vertices.push([0, 0])
+    return vertices
+}
+
+export const count_tails = function(speed: number) : Barb {
+    let flags = ~~(speed / 50)
+    if (flags > 0) {
+        speed = speed - (flags * 50)
+    }
+    let full_barbs = ~~(speed / 10)
+    if (full_barbs > 0) {
+        speed = speed - (full_barbs * 10)
+    }
+    let half_barbs = ~~(speed / 5)
+    return {
+        'flags': flags,
+        'full_barbs': full_barbs,
+        'half_barbs': half_barbs
+    }
+}
 export const speed = function(u : number, v : number) : number {
     return Math.sqrt(u**2 + v**2)
 }
--- wind_barbs/lib/index.spec.ts
+++ wind_barbs/lib/index.spec.ts
@@ -1,77 +1,5 @@
 const expect = require('chai').expect
-import barbs = require('./barbs')
-
-interface Barb {
-    flags: number;
-    full_barbs: number;
-    half_barbs: number;
-}
-
-let draw = function(barb: Barb) {
-    let length = 7
-    let spacing = 0.875
-    let height = 2.8
-
-    // Special case for lone half barb
-    if ((barb.flags === 0) &&
-        (barb.full_barbs === 0) &&
-        (barb.half_barbs === 1)) {
-        let position = -length + (1.5 *spacing)
-        return [
-            [0, 0],
-            [-length, 0],
-            [position, 0],
-            [position - (spacing / 2), height / 2],
-            [position, 0],
-            [0, 0]
-        ]
-    }
-
-    let vertices = []
-    let position = -length
-    vertices.push([0, 0])
-    if (barb.flags > 0) {
-        for (let ib=0; ib<barb.flags; ib++) {
-            vertices.push([position, 0])
-            vertices.push([position + spacing, height])
-            vertices.push([position + (2 * spacing), 0])
-            position += 2 * spacing
-        }
-    }
-    if (barb.full_barbs > 0) {
-        for (let ib=0; ib<barb.full_barbs; ib++) {
-            vertices.push([position, 0])
-            vertices.push([position - spacing, height])
-            vertices.push([position, 0])
-            position += spacing
-        }
-    }
-    if (barb.half_barbs > 0) {
-        vertices.push([position, 0])
-        vertices.push([position - (spacing / 2), height/2])
-        vertices.push([position, 0])
-        position += spacing
-    }
-    vertices.push([0, 0])
-    return vertices
-}
-
-let count_tails = function(speed: number) : Barb {
-    let flags = ~~(speed / 50)
-    if (flags > 0) {
-        speed = speed - (flags * 50)
-    }
-    let full_barbs = ~~(speed / 10)
-    if (full_barbs > 0) {
-        speed = speed - (full_barbs * 10)
-    }
-    let half_barbs = ~~(speed / 5)
-    return {
-        'flags': flags,
-        'full_barbs': full_barbs,
-        'half_barbs': half_barbs
-    }
-}
+const barbs = require('./barbs')
 
 describe('wind_barbs', function() {
     describe('count_tails', function() {
@@ -88,7 +16,7 @@ describe('wind_barbs', function() {
             check(15, {'flags': 0, 'full_barbs': 1, 'half_barbs': 1})
         })
         let check = function(speed, expected) {
-            let actual = count_tails(speed)
+            let actual = barbs.count_tails(speed)
             expect(actual).deep.equal(expected)
         }
     })
@@ -150,7 +78,7 @@ describe('wind_barbs', function() {
             ])
         })
         let check = function(barb: Barb, expected) {
-            let actual = draw(barb)
+            let actual = barbs.draw(barb)
             expect(actual).deep.equal(expected)
         }
     })
