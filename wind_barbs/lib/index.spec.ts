47aed19f6f79066225e6022e8d3fbbec2a3f3ae2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-12 14:04:50 +0000

    migrated to Typescript

--- wind_barbs/lib/index.spec.ts
+++ wind_barbs/lib/index.spec.ts
@@ -1,5 +1,10 @@
 const expect = require('chai').expect
 
+interface Barb {
+    flags: number;
+    full_barbs: number;
+    half_barbs: number;
+}
 
 let wind_barb = function() {
     return [
@@ -8,7 +13,7 @@ let wind_barb = function() {
     ];
 }
 
-let draw = function(barb) {
+let draw = function(barb: Barb) {
     if (barb.full_barbs === 1) {
         return [
             [0, 0],
@@ -28,7 +33,7 @@ let draw = function(barb) {
     ]
 }
 
-let count_flags = function(speed) {
+let count_flags = function(speed: number) : Barb {
     let flags = ~~(speed / 50)
     if (flags > 0) {
         speed = speed - (flags * 50)
@@ -80,7 +85,7 @@ describe('wind_barb', function() {
 
     describe('draw', function() {
         it('should return drawing instructions for half_barb', function() {
-            let actual = draw({'flags': 0, 'full_barb': 0, 'half_barb': 1})
+            let actual = draw({flags: 0, full_barbs: 0, half_barbs: 1})
             let expected = [[0, 0],
                             [-7, 0],
                             [-5.6875, 0],
@@ -90,7 +95,7 @@ describe('wind_barb', function() {
             expect(actual).deep.equal(expected)
         })
         it('should return drawing instructions for full_barb', function() {
-            let actual = draw({'flags': 0, 'full_barb': 1, 'half_barb': 0})
+            let actual = draw({flags: 0, full_barbs: 1, half_barbs: 0})
             let expected = [[0, 0],
                             [-7, 0],
                             [-7.875, 2.8],
