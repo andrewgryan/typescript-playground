232a70e8f28ed91a71ccdd241fdbf9812a2adeea
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-14 10:21:10 +0000

    added a little simulation of advecting winds

--- wind_barbs/main.py
+++ wind_barbs/main.py
@@ -23,8 +23,8 @@ glyph = wind.Barbs(x="x", y="y", u="u", v="v")
 x = np.linspace(-30, 30, 40, dtype="f")
 y = np.linspace(-30, 30, 40, dtype="f")
 X, Y = np.meshgrid(x, y)
-U = X
-V = Y
+U = -15 * np.ones(X.shape)
+V = 20 * np.exp(-(X**2 + Y**2) / 60) + 10 * np.sin((2 * np.pi * X) / 60)
 source = bokeh.models.ColumnDataSource({
         "x": X.flatten(),
         "y": Y.flatten(),
@@ -40,15 +40,31 @@ def periodic():
     y = source.data["y"]
     u = source.data["u"]
     v = source.data["v"]
-    theta = np.pi / 4
-    up = (u * np.cos(theta)) - (v * np.sin(theta))
-    vp = (u * np.sin(theta)) + (v * np.cos(theta))
+    simulate = "forecast"
+    if simulate == "rotation":
+        u, v = rotate_winds(u, v, np.pi / 4)
+    else:
+        x, y, u, v = forecast(x, y, u, v)
     source.data = {
         "x": x,
         "y": y,
-        "u": up,
-        "v": vp
+        "u": u,
+        "v": v
     }
+
+def rotate_winds(u, v, angle):
+    up = (u * np.cos(theta)) - (v * np.sin(theta))
+    vp = (u * np.sin(theta)) + (v * np.cos(theta))
+    return up, vp
+
+def forecast(x, y, u, v):
+    return x, y, u + dydx(u), v + dydx(v)
+
+def dydx(u):
+    du = np.empty(u.shape)
+    du[-1] = u[0] - u[-1]
+    du[:-1] = u[1:] - u[:-1]
+    return du
 
 document = bokeh.plotting.curdoc()
 document.add_root(figure)
